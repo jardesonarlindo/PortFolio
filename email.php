f43a5c84eb81b4d1e5de2bff86034cbe7066b471
Jardeson Arlindo <jardesonaraujoarlindo@gmail.com>
2020-07-18 17:16:56 -0300

    alteração email

--- email.php
+++ email.php
@@ -6,13 +6,13 @@ if(isset($_POST['email']) && !empty($_POST['email'])){
     $message = addcslashes($_POST['message'])
 
     $to = "jardesonaraujoarlindo@gmail.com";
-    $body = "Nome: ".$nome."\r\n"
-            "Email: ".$email."\r\n"
-            "Assunto: ".$subject."\r\n"
+    $body = "Nome: ".$nome."\r\n".
+            "Email: ".$email."\r\n".
+            "Assunto: ".$subject."\r\n".
             "Mensagem: ".$message."\r\n";
-    $header = "From: jardesonaraujoarlindo@gmail.com"."\r\n"
-               ."Reply-To: ".$email."\r\n"
-               ."X=Mailer:PHP/".phpversion();
+    $header = "From: jardesonaraujoarlindo@gmail.com"."\r\n".
+               "Reply-To: ".$email."\r\n".
+               "X=Mailer:PHP/".phpversion();
 
     if(mail($to,$subject,$body,$header)){
         echo "<script type='javascript'>alert('Email enviado com Sucesso!');";
